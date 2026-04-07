f95457c501061ae380833dd5468044ae4dfc20aa
mayakiyomi <mayakiyomi@gmail.com>
2026-04-06 19:21:07 -0700

    portal push

---
+++ script.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    const toggle = document.querySelector('#modeToggle');
+    const body = document.querySelector('body');
+    const sections = document.querySelectorAll('section');
+
+    toggle.addEventListener('change', function() {
+
+        if (toggle.checked) {
+            body.classList.add('switch');
+
+            for (const section of sections) {
+                section.classList.add('switch');
+            }
+
+        } else {
+            body.classList.remove('switch');
+
+            for (const section of sections) {
+                section.classList.remove('switch');
+            }
+        }
+    });
+
+})();
